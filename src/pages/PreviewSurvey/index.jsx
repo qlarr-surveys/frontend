a25f761beaecd52b27d67ca17e52d901c429e9c3
Amr Askoura <askoura@qlarr..com>
2025-04-26 21:53:15 +0200

    apply navigation

--- src/pages/PreviewSurvey/index.jsx
+++ src/pages/PreviewSurvey/index.jsx
@@ -3,7 +3,7 @@ import styles from "./PreviewSurvey.module.css";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import React, { useState } from "react";
 import SurveyIcon from "~/components/common/SurveyIcons/SurveyIcon";
-import { Box, Chip, IconButton, Tab, Tabs } from "@mui/material";
+import { Box, Chip, FormControl, IconButton, InputLabel, MenuItem, Select, Tab, Tabs } from "@mui/material";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
 import { isSurveyAdmin } from "~/constants/roles";
 import { SurveyClone } from "~/components/manage/SurveyClone";
@@ -11,6 +11,7 @@ import { BG_COLOR } from "~/constants/theme";
 import { PREVIEW_MODE, routes } from "~/routes";
 import { Close } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
+import { useSelector } from 'react-redux';
 
 function PreviewSurvey({ guest = false }) {
   const navigate = useNavigate();
@@ -21,7 +22,9 @@ function PreviewSurvey({ guest = false }) {
     searchParams.get("mode") || "online"
   );
 
-  const navigationMode = searchParams.get("navigation_mode");
+  
+
+  const [navigationMode, setNavigationMode] = useState(searchParams.get("navigation_mode") || "GROUP_BY_GROUP");
   const [lang, setLang] = useState(searchParams.get("lang"));
   const surveyId = getparam(useParams(), "surveyId");
 
@@ -46,6 +49,11 @@ function PreviewSurvey({ guest = false }) {
   const handleChange = (event, newValue) => {
     setPreviewMode(newValue);
   };
+  
+  const handleNavigationModeChange = (event) => {
+    setNavigationMode(event.target.value);
+  };
+  
   const [openCloneModal, setOpenCloneModal] = useState(false);
 
   return (
@@ -79,6 +87,7 @@ function PreviewSurvey({ guest = false }) {
         position="relative"
         width="100%"
         justifyContent="center"
+        alignItems="center"
       >
         <Tabs
           value={previewMode}
@@ -95,6 +104,24 @@ function PreviewSurvey({ guest = false }) {
             label={<SurveyIcon name="offline" />}
           />
         </Tabs>
+
+        <Box sx={{ position: 'absolute', right: 16 }}>
+          <FormControl sx={{ minWidth: 200 }} size="small">
+            <InputLabel id="navigation-mode-select-label">Navigation Mode</InputLabel>
+            <Select
+              labelId="navigation-mode-select-label"
+              id="navigation-mode-select"
+              value={navigationMode}
+              onChange={handleNavigationModeChange}
+              label="Navigation Mode"
+            >
+              <MenuItem value="ALL_IN_ONE">All In One</MenuItem>
+              <MenuItem value="GROUP_BY_GROUP">Group By Group</MenuItem>
+              <MenuItem value="QUESTION_BY_QUESTION">Question By Question</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        
         {guest && isSurveyAdmin() && (
           <IconButton
             className={styles.iconButton}
