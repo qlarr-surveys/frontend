a5300b82211d8383d31288ec72241a1848c84154
leutrimahmeti <trimmahmeti@outlook.com>
2025-05-06 02:14:58 +0200

    updated the new dropdown with translations , adjusted the X to go back to design now .

--- src/pages/PreviewSurvey/index.jsx
+++ src/pages/PreviewSurvey/index.jsx
@@ -11,7 +11,6 @@ import { BG_COLOR } from "~/constants/theme";
 import { PREVIEW_MODE, routes } from "~/routes";
 import { Close } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
-import { useSelector } from 'react-redux';
 
 function PreviewSurvey({ guest = false }) {
   const navigate = useNavigate();
@@ -22,7 +21,8 @@ function PreviewSurvey({ guest = false }) {
     searchParams.get("mode") || "online"
   );
 
-  
+  const { t: tDesign } = useTranslation("design");
+
 
   const [navigationMode, setNavigationMode] = useState(searchParams.get("navigation_mode") || "GROUP_BY_GROUP");
   const [lang, setLang] = useState(searchParams.get("lang"));
@@ -49,13 +49,12 @@ function PreviewSurvey({ guest = false }) {
   const handleChange = (event, newValue) => {
     setPreviewMode(newValue);
   };
-  
+
   const handleNavigationModeChange = (event) => {
     setNavigationMode(event.target.value);
   };
-  
-  const [openCloneModal, setOpenCloneModal] = useState(false);
 
+  const [openCloneModal, setOpenCloneModal] = useState(false);
   return (
     <>
       <Box
@@ -66,9 +65,9 @@ function PreviewSurvey({ guest = false }) {
         <Chip
           label={t("preview")}
           color="primary"
-          onDelete={() => navigate(-1)}
+          onDelete={() => navigate(`/design-survey/${surveyId}`)}
           deleteIcon={<Close />}
-          style={{ marginLeft: "auto", marginRight: "auto" }} // Centered
+          style={{ marginLeft: "auto", marginRight: "auto" }} 
         />
       </Box>
       <SurveyClone
@@ -105,23 +104,30 @@ function PreviewSurvey({ guest = false }) {
           />
         </Tabs>
 
-        <Box sx={{ position: 'absolute', right: 16 }}>
-          <FormControl sx={{ minWidth: 200 }} size="small">
-            <InputLabel id="navigation-mode-select-label">Navigation Mode</InputLabel>
+        <Box position="absolute" right="16px" top="0px">
+          <FormControl sx={{ minWidth: 200 }}>
+            <InputLabel id="navigation-mode-select-label">
+              {tDesign("navigation_mode")}
+            </InputLabel>
             <Select
               labelId="navigation-mode-select-label"
               id="navigation-mode-select"
+              size="small"
               value={navigationMode}
               onChange={handleNavigationModeChange}
               label="Navigation Mode"
             >
-              <MenuItem value="ALL_IN_ONE">All In One</MenuItem>
-              <MenuItem value="GROUP_BY_GROUP">Group By Group</MenuItem>
-              <MenuItem value="QUESTION_BY_QUESTION">Question By Question</MenuItem>
+              <MenuItem value="ALL_IN_ONE">{tDesign("all_in_one")}</MenuItem>
+              <MenuItem value="GROUP_BY_GROUP">
+                {tDesign("group_by_group")}
+              </MenuItem>
+              <MenuItem value="QUESTION_BY_QUESTION">
+                {tDesign("question_by_question")}
+              </MenuItem>
             </Select>
           </FormControl>
         </Box>
-        
+
         {guest && isSurveyAdmin() && (
           <IconButton
             className={styles.iconButton}
