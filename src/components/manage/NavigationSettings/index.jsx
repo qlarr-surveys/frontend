ae7efc52001bd19235322f31272564b03d8c2b0e
leutrimahmeti <trimmahmeti@outlook.com>
2025-09-11 15:42:10 +0200

    added tooltips for navigation settings , removed padding so it looks aligned , added a select dropdown for timeSinceStart

--- src/components/manage/NavigationSettings/index.jsx
+++ src/components/manage/NavigationSettings/index.jsx
@@ -7,7 +7,47 @@ import { RHFSelect, RHFSwitch } from "~/components/hook-form";
 import { isSurveyAdmin } from "~/constants/roles";
 import { useDispatch, useSelector } from "react-redux";
 import { NAVIGATION_MODE, SURVEY_STATUS } from "~/constants/survey";
-import { surveyAttributeChanged, surveyAttributeChangedImmediate } from "~/state/edit/editState";
+import {
+  surveyAttributeChanged,
+  surveyAttributeChangedImmediate,
+} from "~/state/edit/editState";
+
+const RESUME_EXPIRY_PRESETS = [
+  { key: "1h", labelKey: "since_start.1h", fallback: "1 hour", ms: 3600000 },
+  { key: "6h", labelKey: "since_start.6h", fallback: "6 hours", ms: 21600000 },
+  { key: "1d", labelKey: "since_start.1d", fallback: "1 day", ms: 86400000 },
+  { key: "3d", labelKey: "since_start.3d", fallback: "3 days", ms: 259200000 },
+  { key: "1w", labelKey: "since_start.1w", fallback: "1 week", ms: 604800000 },
+  {
+    key: "1m",
+    labelKey: "since_start.1m",
+    fallback: "1 month",
+    ms: 2592000000,
+  },
+  {
+    key: "3m",
+    labelKey: "since_start.3m",
+    fallback: "3 months",
+    ms: 7776000000,
+  },
+  {
+    key: "6m",
+    labelKey: "since_start.6m",
+    fallback: "6 months",
+    ms: 15552000000,
+  },
+  {
+    key: "1y",
+    labelKey: "since_start.1y",
+    fallback: "1 year",
+    ms: 31536000000,
+  },
+];
+
+const findPresetByMs = (ms) => RESUME_EXPIRY_PRESETS.find((p) => p.ms === ms);
+const findPresetByKey = (key) =>
+  RESUME_EXPIRY_PRESETS.find((p) => p.key === key);
+
 const NavigationSettings = () => {
   const dispatch = useDispatch();
   const survey = useSelector((state) => state.editState.survey);
@@ -52,6 +92,11 @@ const NavigationSettings = () => {
     skipInvalid: Boolean(survey?.surveyNavigationData?.skipInvalid),
   });
 
+  const [resumePresetKey, setResumePresetKey] = useState(
+    findPresetByMs(survey?.surveyNavigationData?.resumeExpiryMillis)?.key ??
+      "1w"
+  );
+
   const handleToggleChange = (key) => (e) => {
     const next = e.target.checked;
     setToggles((prev) => ({ ...prev, [key]: next }));
@@ -66,6 +111,20 @@ const NavigationSettings = () => {
         value: event.target.value,
       })
     );
+  };
+
+  const onResumeExpiryChanged = (event) => {
+    const key = event.target.value;
+    setResumePresetKey(key);
+    const preset = findPresetByKey(key);
+    if (preset) {
+      dispatch(
+        surveyAttributeChanged({
+          key: "resumeExpiryMillis",
+          value: preset.ms,
+        })
+      );
+    }
   };
 
   return (
@@ -87,6 +146,21 @@ const NavigationSettings = () => {
           {t("all_in_one")}
         </MenuItem>
       </RHFSelect>
+
+      <RHFSelect
+        labelId="resume-expiry-label"
+        value={resumePresetKey}
+        disabled={isDisabled}
+        label={t("since_start.label", "Time since start")}
+        onChange={onResumeExpiryChanged}
+      >
+        {RESUME_EXPIRY_PRESETS.map((p) => (
+          <MenuItem key={p.key} value={p.key}>
+            {t(p.labelKey, p.fallback)}
+          </MenuItem>
+        ))}
+      </RHFSelect>
+
       {toggleDefs.map(({ key, labelKey, tooltipKey }) => (
         <Box key={key} className={styles.boxContainer}>
           <Box display="flex" alignItems="center" gap=".5rem">
