d4517f0ba1645fe43416e8df46413618768da9d2
Omar <111682079+astromar96@users.noreply.github.com>
2026-02-10 22:14:44 +0200

    fix: correct authorization header condition in request interceptor

--- src/services/authenticatedApi.js
+++ src/services/authenticatedApi.js
@@ -14,7 +14,7 @@ const instance = axios.create({
 instance.interceptors.request.use(
   (config) => {
     const token = TokenService.getAuthToken();
-    if (config.url !== REFRESH_URL && config.headers != token) {
+    if (config.url !== REFRESH_URL && token) {
       config.headers["Authorization"] = "Bearer " + token;
     } else {
       delete config.headers["Authorization"];
