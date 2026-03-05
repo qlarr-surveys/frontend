f9c00f24103152314f92f5904d2582c3ef9cf42e
Omar <111682079+astromar96@users.noreply.github.com>
2026-03-05 05:13:35 +0200

    feat: remove unnecessary eslint disable comment in DesignTourProvider

--- src/components/design/DesignTour/index.jsx
+++ src/components/design/DesignTour/index.jsx
@@ -170,7 +170,6 @@ export default function DesignTourProvider({ children }) {
       }
       unclampQuestionTypes();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <>{children}</>;
